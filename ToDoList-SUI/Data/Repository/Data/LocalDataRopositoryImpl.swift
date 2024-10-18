38191e9afce64b0ff88f3c95066a7b32ddad9077
KsArT-IT <ksart.it@gmail.com>
2024-10-18 13:56:51 +0300

    feat: add save tasks to userdefaults

--- ToDoList-SUI/Data/Repository/Data/LocalDataRopositoryImpl.swift
+++ ToDoList-SUI/Data/Repository/Data/LocalDataRopositoryImpl.swift
@@ -12,25 +12,34 @@ final class LocalDataRopositoryImpl: DataRepository {
     private let service: DataService
     
     var updatePublisher: AnyPublisher<String, Never> { service.updatePublisher }
-
+    
     init(service: DataService) {
         self.service = service
     }
     
     func fetchData() async -> Result<[ToDoItem], any Error> {
-        await service.fetchData()
+        let result = await service.fetchData()
+        return switch result {
+        case .success(let items):
+                .success(items.map { $0.mapToItem() })
+        case .failure(let error):
+                .failure(error)
+        }
     }
     
     func fetchData(_ id: String) async -> Result<ToDoItem?, any Error> {
-        await service.fetchData(id)
+        let result = await service.fetchData(id)
+        return switch result {
+        case .success(let item):
+                .success(item?.mapToItem())
+        case .failure(let error):
+                .failure(error)
+        }
     }
     
     func saveData(_ item: ToDoItem) async -> Result<Bool, any Error> {
-        if item.id.isEmpty {
-            await service.saveData(item.copy(id: UUID().uuidString))
-        } else {
-            await service.updateData(item)
-        }
+        let item = if item.id.isEmpty { item.copy(id: UUID().uuidString) } else { item }
+        return await service.saveData(item.mapToDto())
     }
     
     func deleteData(_ id: String) async -> Result<Bool, any Error> {
--- ToDoList-SUI/Data/Service/Data/LocalDataServiceImpl.swift
+++ ToDoList-SUI/Data/Service/Data/LocalDataServiceImpl.swift
@@ -10,31 +10,34 @@ import Combine
 
 final class LocalDataServiceImpl: DataService {
     
-    private var items: [String: ToDoItem] = [:]
+    private var items: [String: ToDoDTO] = [:] {
+        didSet { saveUD() }
+    }
     private let updateSubject = PassthroughSubject<String, Never>()
     public var updatePublisher: AnyPublisher<String, Never> {
         updateSubject.eraseToAnyPublisher()
     }
+    private let keyUD = "ToDoList.key"
     
     init() {
-//        previewData()
+        loadUD()
     }
     
-    func fetchData() async -> Result<[ToDoItem], any Error> {
-        return .success(Array(items.values))
+    func fetchData() async -> Result<[ToDoDTO], any Error> {
+        return .success(getArray())
     }
     
-    func fetchData(_ id: String) async -> Result<ToDoItem?, any Error> {
+    func fetchData(_ id: String) async -> Result<ToDoDTO?, any Error> {
         .success(items[id])
     }
     
-    func saveData(_ item: ToDoItem) async -> Result<Bool, any Error> {
+    func saveData(_ item: ToDoDTO) async -> Result<Bool, any Error> {
         items[item.id] = item
         updateSubject.send(item.id)
         return .success(true)
     }
     
-    func updateData(_ item: ToDoItem) async -> Result<Bool, any Error> {
+    func updateData(_ item: ToDoDTO) async -> Result<Bool, any Error> {
         await saveData(item)
     }
     
@@ -43,18 +46,36 @@ final class LocalDataServiceImpl: DataService {
         return .success(true)
     }
     
-    private func previewData() {
-        (0...100).forEach { num in
-            let item = ToDoItem(
-                id: num.description,
-                date: Date(),
-                title: "Element-\(num)",
-                text: "Go to",
-                isCritical: num % 2 == 0,
-                isCompleted: num % 5 == 0
-            )
-            items[item.id] = item
+    private func loadUD() {
+        Task {
+            guard let data = UserDefaults.standard.data(forKey: keyUD) else { return }
+            do {
+                let list = try JSONDecoder().decode([ToDoDTO].self, from: data)
+                setItems(list)
+            } catch {
+                print("LocalDataServiceImpl: error load data: \(error.localizedDescription)")
+            }
         }
     }
-
+    
+    private func saveUD() {
+        Task {
+            do {
+                let data = try JSONEncoder().encode(getArray())
+                UserDefaults.standard.set(data, forKey: keyUD)
+            } catch {
+                print("LocalDataServiceImpl: error save data: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func getArray() -> Array<ToDoDTO> {
+        Array(items.values)
+    }
+    
+    private func setItems(_ list: Array<ToDoDTO>) {
+        items = list.reduce(into: [String: ToDoDTO]()) { dict, item in
+            dict[item.id] = item
+        }
+    }
 }
