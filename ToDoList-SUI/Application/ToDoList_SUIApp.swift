31e284b79acdb81a47a9fd7aaefd80b30c63e609
KsArT-IT <ksart.it@gmail.com>
2024-10-18 17:03:15 +0300

    feat: init firebase swift package

--- ToDoList-SUI/Application/ToDoList_SUIApp.swift
+++ ToDoList-SUI/Application/ToDoList_SUIApp.swift
@@ -6,15 +6,28 @@
 //
 
 import SwiftUI
-import SwiftData
+import FirebaseCore
 
 @main
 struct ToDoList_SUIApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(for: ToDoTable.self)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
 }
