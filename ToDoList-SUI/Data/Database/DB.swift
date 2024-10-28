72ade3a44a56b3ea77a401a87dd5784ad48f1b1e
KsArT-IT <ksart.it@gmail.com>
2024-10-28 23:25:54 +0200

    feat: add firestore rules

--- ToDoList-SUI/Data/Database/DB.swift
+++ ToDoList-SUI/Data/Database/DB.swift
@@ -10,15 +10,35 @@ import Foundation
 
 enum DB {
     
-    enum Todo {
-        static let name = "todo"
-        
-        enum Fields {
-            static let date = "date"
-        }
-    }
-    
     enum Users {
         static let name = "users"
     }
+    
+    enum Todos {
+        static let name = "todos"
+    }
+    
 }
+
+/*
+rules_version = '2';
+
+service cloud.firestore {
+  match /databases/{database}/documents {
+
+    // Ограничиваем доступ к коллекции users
+    match /users/{userId} {
+      
+      // Чтение и запись данных пользователя возможны только для авторизованного пользователя
+      allow read, write: if request.auth != null && request.auth.uid == userId;
+      
+      // Ограничиваем доступ к вложенной коллекции todos
+      match /todos/{todoId} {
+        
+        // Доступ к задачам только для авторизованного пользователя
+        allow read, write: if request.auth != null && request.auth.uid == userId;
+      }
+    }
+  }
+}
+*/
